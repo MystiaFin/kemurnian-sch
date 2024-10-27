ea2b14459002e2901a44899cdf4783160936efc5
MystiaFin <cytrsx01@gmail.com>
2024-10-27 18:01:00 +0700

    install firebase

--- src/app/admin/p/sliders/page.tsx
+++ src/app/admin/p/sliders/page.tsx
@@ -1,7 +1,10 @@
 export default function Slider() {
-    return(
-        <div>
-            <h1>Slider</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Home Page Sliders</h2>
+      <button className="bg-secondary-red text-white w-[30px] h-[30px] rounded-xl mt-2 mb-2">
+        +
+      </button>
+    </div>
+  );
 }
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db, analytics };
