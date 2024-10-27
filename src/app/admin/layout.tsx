f61eca8651540081d47e047888e9c1dbf92e4a38
MystiaFin <cytrsx01@gmail.com>
2024-10-27 20:45:37 +0700

    pls fix admin account later

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,7 @@
+// src/app/admin/layout.tsx
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, getIdTokenResult } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
 import Sidebar from "./components/sidebar";
 import type { Metadata } from "next";
 
@@ -6,6 +10,36 @@ export const metadata: Metadata = {
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const tokenResult = await getIdTokenResult(user);
+        if (tokenResult.claims.admin) {
+          setIsAuthenticated(true);
+          setIsAdmin(true);
+        } else {
+          setIsAuthenticated(true);
+          setIsAdmin(false);
+        }
+      } else {
+        window.location.href = "/admin-login";
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAdmin) {
+    return <div>Access Denied</div>;
+  }
+
   return (
     <div className="flex">
       <Sidebar />
--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,27 +1,20 @@
-// Import the functions you need from the SDKs you need
+// src/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, analytics };
+export { auth, db };
