13b824148cb465e9588499989d0fe5442118511d
MystiaFin <cytrsx01@gmail.com>
2024-12-24 10:51:15 +0700

    new concept

---
+++ src/components/HomeNavbar.tsx
@@ -0,0 +1,20 @@
+const HomeNavbar = () => {
+  return (
+    <nav className="flex bg-overlay-gray">
+      <button className="text-secondary-red bg-white p-2 border-b-4 border-secondary-red">
+        Home
+      </button>
+      <button className="p-2 border border-solid border-gray-400">
+        Kemurnian
+      </button>
+      <button className="p-2 border border-solid border-gray-400">
+        Kemurnian II
+      </button>
+      <button className="p-2 border border-solid border-gray-400">
+        Kemurnian III
+      </button>
+    </nav>
+  );
+};
+
+export default HomeNavbar;
